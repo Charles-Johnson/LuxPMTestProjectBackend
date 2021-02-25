20e13a425e21d489139cf30502808df91c380e53
Charles Johnson <charlesthomasjohnson0@gmail.com>
2021-02-25 15:47:01 +0900

    :card_file_box: Use Schema add method to add more db fields

--- models/user.model.js
+++ models/user.model.js
@@ -7,11 +7,14 @@ var UserSchema = new mongoose.Schema({
     createdAt: { type: Date, required: true, default: Date.now },
     updatedAt: { type: Date, required: true, default: Date.now },
     hash: { type: String, required: true },
-    salt: { type: String, required: true },
+    salt: { type: String, required: true }
+}, { discriminatorKey: 'role', toJSON: { getters: true } });
+
+UserSchema.add({
     firstname: {type: String, required: true},
     lastname: {type: String, required: true},
-    phoneNumber: {type: String, required: true},
-}, { discriminatorKey: 'role', toJSON: { getters: true } });
+    phoneNumber: {type: String, required: true}
+});
 
 UserSchema.methods.setPassword = function (password) {
     this.salt = crypto.randomBytes(16).toString('hex');
